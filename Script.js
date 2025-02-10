292d0c126f3ae32ff04d15cfb2e3a26bf6a8eeb1
GrupoEl <contael734@gmail.com>
2025-02-10 20:34:09 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,46 @@
+function MenuShow(){
+    let MenuAPR = document.querySelector(".MenuAP")
+    if(MenuAPR.classList.contains("open")){
+        MenuAPR.classList.remove("open");
+        document.querySelector(".MenuIcon").src="./Icon-Menu.svg"
+    }else{MenuAPR.classList.add("open")
+         document.querySelector(".MenuIcon").src="./Menu-Icon-x.svg"
+    }
+}
+
+let MySeach = document.querySelector(".PesquisaCont")
+let inputBox = MySeach.querySelector("input")
+let Redirect = MySeach.querySelector("a")
+let List = MySeach.querySelector("list")
+let IconSeach = document.querySelector(".IconSeach")
+let link 
+
+inputBox.onkeyup = (e)=>{
+    let UseData = e.target.value; 
+    let ArrayList = []; 
+
+    if(e.key ==="Enter"){
+       if(UseData) {
+        window.open(href= `https://www.google.com/search?q=${UseData}`, "_blank")
+       }
+    }
+
+    if(UseData){
+        IconSeach.onclick = ()=>{
+            link = `https://www.google.com/search?q=${UseData}`
+            Redirect.setAttribute("href", link)
+            Redirect.click()
+        }
+
+        ArrayList = sugestoes.filter((data)=>{
+            return data.toLocaleLowerCase().startsWith(UseData.toLocaleLowerCase())
+        })
+
+        ArrayList= ArrayList.map((data)=>{
+            return data = `<li>${data}</li>`
+        })
+
+        MySeach.classList.add("active")
+        }
+       
+}
